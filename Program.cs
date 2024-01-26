d2a3c2053e295d506c9ee46823f6fa1244aedc9c
Renan Marcelino Da Silva Souza <renanmarcelinodass@gmail.com>
2024-01-26 09:43:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class Twitter
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o texto a ser convertido: ");
+        string texto = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(texto))
+        {
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+            string textoConvertido = textInfo.ToTitleCase(texto);
+            Console.WriteLine(textoConvertido);
+        }
+        else
+        {
+            Console.WriteLine("Texto vazio. Nada a converter.");
+        }
+    }
+}
---
+++ TesteDoJob/UltimoTeste/Program.cs
@@ -0,0 +1,43 @@
+﻿// edhfbbefijiebf ewbfv ewfuub eiuufb uebfiew uwebfi
+// jbij usdff wei oieww foi djj ewjew sdj fjj sd ej
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num1 = 7;
+        double num2 = 3;
+        double num3 = 5;
+
+        double maior = num1;
+
+        if (num2 > num1)
+        {
+            maior = num2;
+        }
+        if (num3 > num1)
+        {
+            maior = num3;
+        }
+    
+        double menor = num2;
+
+        if(num1 < num2)
+        {
+            menor = num1;
+        }
+        if(num3 < num2)
+        {
+            menor = num3;
+        }
+
+        double media = (num1 + num2 + num3) / 3;
+        
+        Console.WriteLine(maior);
+        Console.WriteLine(menor);
+        Console.WriteLine(media);
+    }
+
+}
